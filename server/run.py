45b95ea18cce831704385ff95ccdde65b688104b
Aitor Miguel Blanco <aitormibl@gmail.com>
2022-07-01 18:06:12 +1000

    Initial server setup.

---
+++ server/run.py
@@ -0,0 +1,5 @@
+"""
+    Web server for the speech learning task app.
+"""
+
+# TODO
---
+++ server/slack_bot.py
@@ -0,0 +1,21 @@
+"""
+    Slack bot to publish the server hostname.
+"""
+
+import os
+
+from slack import WebClient
+
+
+def publish_hostname() -> None:
+    host = os.popen("hostname -I | awk '{print $1;}'").read()[0:-1]
+    token = os.environ.get('SLACK_TOKEN')
+    if token is not None:
+        slack_client = WebClient(token)
+        slack_client.chat_postMessage(
+            channel="reading_to_robot",
+            text="Access Reading To Robot App in: http://{}:5000".format(host))
+
+
+if __name__ == '__main__':
+    publish_hostname()
