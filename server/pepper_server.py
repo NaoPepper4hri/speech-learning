34789d6f942608f2134a1421e954b6a06aa007f1
Aitor Miguel Blanco <aitormibl@gmail.com>
2022-07-05 15:58:42 +1000

    Set up minimal grpc/flask server

---
+++ server/pepper_server.py
@@ -0,0 +1,57 @@
+from concurrent import futures
+from queue import Queue, Empty
+from typing import AsyncIterable
+
+import grpc
+import pepper_command_pb2_grpc
+import pepper_command_pb2
+from google.protobuf import empty_pb2
+
+
+class CommandBridge():
+    _instance = None
+
+    def __new__(cls):
+        if not isinstance(cls._instance, cls):
+            cls._instance = super(CommandBridge, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.queue = Queue()
+        self.accepting_cmds = True
+
+    def stop(self):
+        self.accepting_cmds = False
+
+    def send_command(self, cmd: str) -> None:
+        self.queue.put({"movement": cmd, "halt": True})
+
+    def get(self, timeout: float) -> pepper_command_pb2.Command:
+        cmd = self.queue.get(timeout=timeout)
+        return pepper_command_pb2.Command(movement=cmd["movement"], halt_last=cmd["halt"])
+
+
+class PepperServicer(pepper_command_pb2_grpc.PepperServicer):
+    QUEUE_TIMEOUT = 1
+
+    def __init__(self):
+        super().__init__()
+        self.queue = CommandBridge()
+
+    def ListenMovementCommand(
+            self,
+            request: empty_pb2.Empty,
+            context) -> AsyncIterable[pepper_command_pb2.Command]:
+        while self.queue.accepting_cmds:
+            try:
+                yield self.queue.get(timeout=self.QUEUE_TIMEOUT)
+            except Empty:
+                continue
+
+
+def serve_grpc() -> None:
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    pepper_command_pb2_grpc.add_PepperServicer_to_server(PepperServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
--- server/run.py
+++ server/run.py
@@ -2,4 +2,29 @@
     Web server for the speech learning task app.
 """
 
-# TODO
+from flask import Flask, request
+from threading import Thread
+
+from pepper_server import serve_grpc, CommandBridge
+from slack_bot import publish_hostname
+
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/')
+def home():
+    return app.send_static_file('index.html')
+
+
+@app.route('/pubCommand', methods=['POST'])
+def pubCommand():
+    req = request.get_json()
+    cb = CommandBridge()
+    cb.send_command({"movement": req, "halt": True})
+
+
+if __name__ == '__main__':
+    publish_hostname()
+    grpc = Thread(target=serve_grpc)
+    grpc.start()
+    app.run(host='0.0.0.0')
