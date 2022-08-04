b287a63078f85210da42f4d7c586cd42d2ad6dec
Aitor Miguel Blanco <aitormibl@gmail.com>
2022-08-04 14:23:13 +1000

    Updated installation method.

--- README.md
+++ README.md
@@ -4,36 +4,36 @@ experiment app
 
 # Installation in Raspberry Pi:
 
-1. Copy to server with ip `raspberry-ip`:
+You will need to have access to a system with a bash shell, ssh and node installed.
+If you are in linux, you probably are good with the defaults, for Windows users, your easiest way would be using the
+Git Bash program to run the following commands.
 
+1. `cd` into the location of the repository in your machine.
 ```
-rsync -a speech-learning ubuntu@{raspberry-ip}:~
-```
-
-2. Login into the server:
-
-```
-ssh ubuntu@{raspberry-ip}
+cd ~/speech-learning
 ```
 
-3. Run installation script:
-
+2. Execute the following script:
 ```
-~/speech-learning/install.sh
+./install.sh
 ```
 
-4. When you restart the raspberry, the webapp should be setup.
+3. You will be prompted with some parameters to fill up (like user and ip for the raspberry pi).
+You will also be asked for the password for the raspberry pi user several times, this is normal.
+Please note that our lab's configuration is set up as default.
+You can change any entries or simply press enter when asked for input to follow the defaults.
+
+4. If everything went well, you should now be able to access the server from your web browser.
 
 # Troubleshooting
 If the webapp is not working, there might be some error in the server.
-To check wether there is the case, you can first open the `/logs/{app-name}.err.log` file in the
+To check wether there is the case, you can first open the `/logs/speech-practice.err.log` file in the
 raspberry pi.
 You might see an error ocurring on the appserver there.
 
-When you try to fix the error, you can test the server without installation by running this
-command:
+When you try to fix the error, you can test the server without installation by running this command in the raspberry pi:
 ```
-python3 ~/duolingo-webapp/server/run.py
+python3 ~/speech-practice/run.py
 ```
 This will run the server and print the output to the terminal, so you can see any errors happening
 live.
--- install.sh
+++ install.sh
@@ -4,23 +4,56 @@
 src_path=$(dirname $(realpath $0))
 server_name=$(basename $src_path)
 
-# Build npm
+read -p "Enter slackbot token, if any, otherwise leave empty: " SLACK_TOKEN
+read -p "Target ip address (default: 10.3.141.1) " TARGET_IP
+read -p "Target user (default: ubuntu):" TARGET_USER
+
+if [ -z "$TARGET_IP" ]
+then
+    TARGET_IP=10.3.141.1
+fi
+if [ -z "$TARGET_USER" ]
+then
+    TARGET_USER=ubuntu
+fi
+echo "Using target: $TARGET_USER@$TARGET_IP"
+echo "$TARGET_USER@$TARGET_IP's password: "
+read -s PASSWORD
+
+
+# Build client and send to target machine
 cd $src_path/client
 npm run build
+cd $src_path
+rsync -a server/ $TARGET_USER@$TARGET_IP:~/$server_name
+
+# Install required python packages
+read -r -p "Update required Python packages? [Y/n] " response
+case "$response" in
+    [nN][oO]|[nN])
+        ;;
+    *)
+        requirements=`cat $src_path/requirements.txt | tr '\n' ' '`
+        echo $PASSWORD | ssh -tt $TARGET_USER@$TARGET_IP "sudo python3 -m pip install $requirements"
+        ;;
+esac
 
 # Set up supervisorctl
-read -p "Enter slackbot token (to publish server ip in Slack): " SLACK_TOKEN
+read -r -p "Reconfigure Supervisor [Y/n] " response
+case "$response" in
+    [nN][oO]|[nN]) ;;
+    *)
+        supervisor_conf="[program:${server_name}]
+        stopsignal=INT
+        command=/usr/bin/python3 /home/$TARGET_USER/$server_name/run.py
+        autostart=true
+        autorestart=true
+        stderr_logfile=/logs/${server_name}.err.log
+        stdout_logfile=/logs/${server_name}.out.log
+        environment=SLACK_TOKEN=\\\"${SLACK_TOKEN}\\\"
+        "
 
-echo "[program:${server_name}]
-stopsignal=INT
-command=/usr/bin/python3 ${src_path}/server/run.py
-autostart=true
-autorestart=true
-stderr_logfile=/logs/${server_name}.err.log
-stdout_logfile=/logs/${server_name}.out.log
-environment=SLACK_TOKEN=\"${SLACK_TOKEN}\"
-" > /etc/supervisor/conf.d/$server_name.conf
-
-supervisorctl reread
-supervisorctl update
+        echo $PASSWORD | ssh -tt $TARGET_USER@$TARGET_IP "echo \"$supervisor_conf\" | sudo tee -a  /etc/supervisor/conf.d/$server_name.conf >/dev/null && sudo supervisorctl reread && sudo supervisorctl update"
+        ;;
+esac
 
