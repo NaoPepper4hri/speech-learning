97f055c6edbb6ced20925f5a52bc7e7e41437006
Aitor Miguel Blanco <aitormibl@gmail.com>
2022-08-31 13:56:03 +0200

    Control page formatting.

--- client/src/PepperControl.js
+++ client/src/PepperControl.js
@@ -1,12 +1,17 @@
 import {
   Button,
+  Card,
+  CardActionArea,
+  CardContent,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   Divider,
   Grid,
+  Stack,
   TextField,
+  Typography,
 } from "@mui/material";
 import React from "react";
 import {
@@ -19,25 +24,50 @@ import {
   restartExperiment,
 } from "./utils";
 
-const actions = [
-  "Ok, let's try one more task and see if we can improve!",
-  "Well, you know what they say, practice makes perfect.",
-  "I'm glad to hear that, lets keep going.",
-  "I'm glad to hear that, lets keep going.",
-  "You got most of those correct, great job.",
-  "Lets see if we can do even better and faster in the next block.",
-  "Sorry I didn't get that, can you repeat what you just said.",
-  "Ok, I got that.",
-  "It was really fun learning Spanish with you.",
-  "This was really fun. Thanks for learning Spanish with me.",
-  "Thanks for doing the extra bonus round with me. I hope you had a good time, it was fun learning Spanish with you.",
+const scheduled_actions = [
+  [{ text: "Ok, let's try one more task and see if we can improve!" }],
+  [{ text: "Well, you know what they say, practice makes perfect." }],
+  [{ text: "You’re really making good progress, hang in there." }],
+  [
+    {
+      text: "Let's see if we can do even better and faster in the next block.",
+    },
+  ],
+  [
+    { text: "You got most of those correct, great job.", label: "continue" },
+    { text: "How confident are you about learning Spanish now?", label: "end" },
+  ],
+  [{ text: "You got most of those correct, great job." }],
 ];
+
+const other_actions = [
+  "I didn't hear a response, do you need me to repeat the question?",
+  "Sorry I didn't get that, can you repeat what you just said?",
+  "Ok, I got that.",
+  "This was really fun. Thanks for learning Spanish with me.",
+  "Thanks for doing the extra bonus round with me, I hope you had a good time. It was fun " +
+    "learning Spanish with you.",
+];
+
+class CardButton extends React.Component {
+  render() {
+    const { children, onClick } = this.props;
+    return (
+      <Card sx={{ border: "1px solid black" }}>
+        <CardActionArea onClick={onClick}>
+          <CardContent>{children}</CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  }
+}
 
 class PepperControl extends React.Component {
   state = {
     dialogOpen: false,
     participantId: "",
     dyntext: "",
+    notes: "",
     id: "",
   };
 
@@ -49,111 +79,136 @@ class PepperControl extends React.Component {
     }
   };
 
-  render() {
-    const { id, dyntext } = this.state;
+  wOzStageReaction = (r, key, ncols = 12) => {
+    const cols = ncols / r.length;
+    console.log(ncols, r, r.length, cols);
+    return r.map((t, idx) => (
+      <Grid key={`r${key}c${idx}`} item xs={cols} justifyContent="center">
+        <CardButton>
+          <Stack direction="row" spacing={3}>
+            <Typography key="idx">
+              <strong>{`${key}${t.label ? `.- ${t.label}:` : "."}`}</strong>
+            </Typography>
+            <Divider key="divider" orientation="vertical" flexItem />
+            <Typography key="text">{t.text}</Typography>
+          </Stack>
+        </CardButton>
+      </Grid>
+    ));
+  };
+
+  insertText = (value, hint, onSubmit) => {
     return (
-      <Grid container padding={5} spacing={3} alignItems="center">
+      <React.Fragment>
         <Grid item xs={1} justifyContent="center">
-          <Button variant="outlined" onClick={() => requestPepperText(dyntext)}>
-            Say text
-          </Button>
+          <CardButton onClick={onSubmit}>
+            <Typography>{hint}</Typography>
+          </CardButton>
         </Grid>
         <Grid item xs={11}>
           <TextField
-            label="Insert text"
+            label="Insert text (send with `ctrl + Enter`)"
             variant="outlined"
             fullWidth
             multiline
-            value={dyntext}
+            value={value}
             onKeyPress={(ev) => {
               if (ev.shiftKey && ev.key === "Enter") {
-                requestPepperText(dyntext);
+                onSubmit();
               }
             }}
             onChange={(event) => this.setState({ dyntext: event.target.value })}
           />
         </Grid>
-        {actions.map((a, idx) => (
-          <Grid key={idx} item xs={3} justifyContent="center">
-            <Button
-              variant="outlined"
-              onClick={() => {
-                requestPepperText(a);
-              }}
-            >
-              {a}
-            </Button>
+      </React.Fragment>
+    );
+  };
+
+  render() {
+    const { id, dyntext, notes } = this.state;
+    return (
+      <React.Fragment>
+        <Grid container padding={5} spacing={3} alignItems="center">
+          {this.insertText(dyntext, "Say text!", () => {
+            requestPepperText(dyntext);
+          })}
+          {scheduled_actions.map((a, idx) => this.wOzStageReaction(a, idx))}
+          <Grid item xs={12}>
+            <Divider />
           </Grid>
-        ))}
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            variant="outlined"
-            onClick={() => requestPepperLookAtParticipant()}
+          {other_actions.map((a, idx) => (
+            <Grid key={idx} item xs={4} justifyContent="center">
+              <CardButton
+                onClick={() => {
+                  requestPepperText(a);
+                }}
+              >
+                <Typography>{a}</Typography>
+              </CardButton>
+            </Grid>
+          ))}
+          <Grid item xs={12}>
+            <Divider />
+          </Grid>
+          <Grid item container direction="row" spacing={1}>
+            <Grid item>
+              <CardButton onClick={() => setConversationDone()}>
+                <Typography>Conversation Done</Typography>
+              </CardButton>
+            </Grid>
+            <Grid item>
+              <CardButton onClick={() => requestPepperLookAtParticipant()}>
+                <Typography>Look at participant</Typography>
+              </CardButton>
+            </Grid>
+            <Grid item>
+              <CardButton onClick={() => requestPepperLookAtScreen()}>
+                <Typography>Look at screen</Typography>
+              </CardButton>
+            </Grid>
+            <Grid item>
+              <CardButton onClick={() => saveData()}>
+                <Typography>Save data</Typography>
+              </CardButton>
+            </Grid>
+            <Grid item>
+              <CardButton onClick={() => this.setState({ dialogOpen: true })}>
+                <Typography>Restart experiment</Typography>
+              </CardButton>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Divider />
+          </Grid>
+          <Grid
+            container
+            item
+            spacing={2}
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
           >
-            Look at participant
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            variant="outlined"
-            onClick={() => requestPepperLookAtScreen()}
-          >
-            Look at screen
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="outlined" onClick={() => setConversationDone()}>
-            Interaction Done
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
-        <Grid
-          container
-          item
-          spacing={2}
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="center"
-        >
-          <Grid item>
-            <TextField
-              label="Participant ID"
-              variant="outlined"
-              value={id}
-              onChange={(event) => this.setState({ id: event.target.value })}
-            />
+            <Grid item>
+              <TextField
+                label="Participant ID"
+                variant="outlined"
+                value={id}
+                onChange={(event) => this.setState({ id: event.target.value })}
+              />
+            </Grid>
+            <Grid item>
+              <CardButton
+                onClick={() => {
+                  initialize({ id: id, date: new Date().toLocaleString() });
+                }}
+              >
+                <Typography>Initialize</Typography>
+              </CardButton>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Button
-              variant="outlined"
-              onClick={() => {
-                initialize({ id: id, date: new Date().toLocaleString() });
-              }}
-            >
-              Initialize
-            </Button>
-          </Grid>
-          <Grid item>
-            <Button variant="outlined" onClick={() => saveData()}>
-              Save data
-            </Button>
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            variant="outlined"
-            onClick={() => this.setState({ dialogOpen: true })}
-          >
-            Restart experiment
-          </Button>
+          {this.insertText(notes, "Send notes!", () => {
+            // TODO: backend method for note taking
+          })}
         </Grid>
         <Dialog
           open={this.state.dialogOpen}
@@ -178,7 +233,7 @@ class PepperControl extends React.Component {
             </DialogActions>
           </DialogContent>
         </Dialog>
-      </Grid>
+      </React.Fragment>
     );
   }
 }
--- client/src/components/StartPage.js
+++ client/src/components/StartPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Box, Fab, Grid, Typography } from "@mui/material";
 import { KeyboardArrowRightRounded } from "@mui/icons-material";
-import { initialize } from "../utils";
 
 class StartPage extends React.Component {
   render() {
