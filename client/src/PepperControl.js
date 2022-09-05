43291d37fe8e83d8ed45f23daea7fe011d125f73
Aitor Miguel Blanco <aitormibl@gmail.com>
2022-09-05 18:15:01 +0200

    Formatted control page

--- client/src/PepperControl.js
+++ client/src/PepperControl.js
@@ -52,14 +52,115 @@ const other_actions = [
 
 class CardButton extends React.Component {
   render() {
-    const { children, onClick } = this.props;
+    const { children, onClick, background } = this.props;
     return (
-      <Card sx={{ border: "1px solid black" }}>
+      <Card sx={{ border: "1px solid black", background: background }}>
         <CardActionArea onClick={onClick}>
           <CardContent>{children}</CardContent>
         </CardActionArea>
       </Card>
     );
+  }
+}
+
+class InsertTextField extends React.Component {
+  render() {
+    const { onSubmit, hint, value, onChange, background } = this.props;
+    console.log("bg", background);
+    return (
+      <React.Fragment>
+        <Grid item xs={1} justifyContent="center">
+          <CardButton onClick={onSubmit} background={background}>
+            <Typography>{hint}</Typography>
+          </CardButton>
+        </Grid>
+        <Grid item xs={11}>
+          <TextField
+            label="Insert text (send with `ctrl + Enter`)"
+            variant="outlined"
+            fullWidth
+            multiline
+            value={value}
+            sx={{ background: `${background}88` }}
+            onKeyPress={(ev) => {
+              if (ev.ctrlKey && ev.key === "Enter") {
+                onSubmit();
+              }
+            }}
+            onChange={onChange}
+          />
+        </Grid>
+      </React.Fragment>
+    );
+  }
+}
+
+class WofOzReaction extends React.Component {
+  state = {
+    value: "",
+  };
+
+  render() {
+    const { value } = this.state;
+    const { response, idx: key } = this.props;
+    var { ncols } = this.props;
+    ncols = ncols ? ncols : 12;
+    console.log("key", key);
+
+    return response.map((t, idx) => (
+      <Grid key={`r${key}c${idx}`} container item spacing={1} xs={ncols}>
+        <Grid item xs={ncols / 2} justifyContent="center">
+          <CardButton
+            onClick={() => {
+              requestPepperText(t.text);
+            }}
+            background="#ffffcc"
+          >
+            <Stack direction="row" spacing={3}>
+              <Typography key="idx">
+                <strong>{`${key}${t.label ? `.- ${t.label}:` : "."}`}</strong>
+              </Typography>
+              <Divider key="divider" orientation="vertical" flexItem />
+              <Typography key="text">{t.text}</Typography>
+            </Stack>
+          </CardButton>
+        </Grid>
+        <Grid item xs={ncols / 4}>
+          <TextField
+            label="Insert text (send with `ctrl + Enter`)"
+            variant="outlined"
+            fullWidth
+            multiline
+            sx={{ background: "#ccffcc88" }}
+            value={value}
+            onKeyPress={(ev) => {
+              if (ev.ctrlKey && ev.key === "Enter") {
+                sendComment(value);
+                this.setState({ value: "" });
+              }
+            }}
+            onChange={(event) => this.setState({ value: event.target.value })}
+          />
+        </Grid>
+        <Grid item xs={ncols / 4}>
+          <TextField
+            label="Insert rating (1-3): (send with `ctrl + Enter`)"
+            variant="outlined"
+            fullWidth
+            multiline
+            sx={{ background: "#ccecff88" }}
+            value={value}
+            onKeyPress={(ev) => {
+              if (ev.ctrlKey && ev.key === "Enter") {
+                sendComment(value);
+                this.setState({ value: "" });
+              }
+            }}
+            onChange={(event) => this.setState({ value: event.target.value })}
+          />
+        </Grid>
+      </Grid>
+    ));
   }
 }
 
@@ -80,117 +181,11 @@ class PepperControl extends React.Component {
     }
   };
 
-  wOzStageReaction = (r, key, ncols = 12) => {
-    const cols = ncols / r.length;
-    console.log(ncols, r, r.length, cols);
-    return r.map((t, idx) => (
-      <Grid key={`r${key}c${idx}`} item xs={cols} justifyContent="center">
-        <CardButton
-          onClick={() => {
-            requestPepperText(t.text);
-          }}
-        >
-          <Stack direction="row" spacing={3}>
-            <Typography key="idx">
-              <strong>{`${key}${t.label ? `.- ${t.label}:` : "."}`}</strong>
-            </Typography>
-            <Divider key="divider" orientation="vertical" flexItem />
-            <Typography key="text">{t.text}</Typography>
-          </Stack>
-        </CardButton>
-      </Grid>
-    ));
-  };
-
-  insertText = (value, hint, onSubmit, onChange) => {
-    return (
-      <React.Fragment>
-        <Grid item xs={1} justifyContent="center">
-          <CardButton onClick={onSubmit}>
-            <Typography>{hint}</Typography>
-          </CardButton>
-        </Grid>
-        <Grid item xs={11}>
-          <TextField
-            label="Insert text (send with `ctrl + Enter`)"
-            variant="outlined"
-            fullWidth
-            multiline
-            value={value}
-            onKeyPress={(ev) => {
-              if (ev.ctrlKey && ev.key === "Enter") {
-                onSubmit();
-              }
-            }}
-            onChange={onChange}
-          />
-        </Grid>
-      </React.Fragment>
-    );
-  };
-
   render() {
     const { id, dyntext, notes } = this.state;
     return (
       <React.Fragment>
-        <Grid container padding={5} spacing={3} alignItems="center">
-          {this.insertText(
-            dyntext,
-            "Say text!",
-            () => {
-              requestPepperText(dyntext);
-              this.setState({ dyntext: "" });
-            },
-            (event) => this.setState({ dyntext: event.target.value })
-          )}
-          {scheduled_actions.map((a, idx) => this.wOzStageReaction(a, idx))}
-          <Grid item xs={12}>
-            <Divider />
-          </Grid>
-          {other_actions.map((a, idx) => (
-            <Grid key={idx} item xs={4} justifyContent="center">
-              <CardButton
-                onClick={() => {
-                  requestPepperText(a);
-                }}
-              >
-                <Typography>{a}</Typography>
-              </CardButton>
-            </Grid>
-          ))}
-          <Grid item xs={12}>
-            <Divider />
-          </Grid>
-          <Grid item container direction="row" spacing={1}>
-            <Grid item>
-              <CardButton onClick={() => setConversationDone()}>
-                <Typography>Conversation Done</Typography>
-              </CardButton>
-            </Grid>
-            <Grid item>
-              <CardButton onClick={() => requestPepperLookAtParticipant()}>
-                <Typography>Look at participant</Typography>
-              </CardButton>
-            </Grid>
-            <Grid item>
-              <CardButton onClick={() => requestPepperLookAtScreen()}>
-                <Typography>Look at screen</Typography>
-              </CardButton>
-            </Grid>
-            <Grid item>
-              <CardButton onClick={() => saveData()}>
-                <Typography>Save data</Typography>
-              </CardButton>
-            </Grid>
-            <Grid item>
-              <CardButton onClick={() => this.setState({ dialogOpen: true })}>
-                <Typography>Restart experiment</Typography>
-              </CardButton>
-            </Grid>
-          </Grid>
-          <Grid item xs={12}>
-            <Divider />
-          </Grid>
+        <Grid container padding={5} spacing={1.5} alignItems="center">
           <Grid
             container
             item
@@ -205,6 +200,7 @@ class PepperControl extends React.Component {
                 variant="outlined"
                 value={id}
                 onChange={(event) => this.setState({ id: event.target.value })}
+                sx={{ background: "#ffcccc88" }}
               />
             </Grid>
             <Grid item>
@@ -212,20 +208,97 @@ class PepperControl extends React.Component {
                 onClick={() => {
                   initialize({ id: id, date: new Date().toLocaleString() });
                 }}
+                background={"#ffcccc"}
               >
                 <Typography>Initialize</Typography>
               </CardButton>
             </Grid>
+            <Grid item>
+              <CardButton
+                onClick={() => setConversationDone()}
+                background={"#ffcccc"}
+              >
+                <Typography>Conversation Done</Typography>
+              </CardButton>
+            </Grid>
+            <Grid item>
+              <CardButton onClick={() => saveData()} background={"#ffcccc"}>
+                <Typography>Save data</Typography>
+              </CardButton>
+            </Grid>
+            <Grid item>
+              <CardButton
+                onClick={() => this.setState({ dialogOpen: true })}
+                background={"#ffcccc"}
+              >
+                <Typography>Restart experiment</Typography>
+              </CardButton>
+            </Grid>
           </Grid>
-          {this.insertText(
-            notes,
-            "Send notes!",
-            () => {
+          <Grid item container direction="row" spacing={1}>
+            <Grid item>
+              <CardButton
+                onClick={() => requestPepperLookAtParticipant()}
+                background={"#ffd3a7"}
+              >
+                <Typography>Look at participant</Typography>
+              </CardButton>
+            </Grid>
+            <Grid item>
+              <CardButton
+                onClick={() => requestPepperLookAtScreen()}
+                background={"#ffd3a7"}
+              >
+                <Typography>Look at screen</Typography>
+              </CardButton>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Divider />
+          </Grid>
+          <InsertTextField
+            value={dyntext}
+            hint="Say text!"
+            onSubmit={() => {
+              requestPepperText(dyntext);
+              this.setState({ dyntext: "" });
+            }}
+            onChange={(event) => this.setState({ dyntext: event.target.value })}
+          />
+          <Grid item xs={12}>
+            <Divider />
+          </Grid>
+          {scheduled_actions.map((a, idx) => (
+            <WofOzReaction response={a} idx={idx} />
+          ))}
+          <Grid item xs={12}>
+            <Divider />
+          </Grid>
+          {other_actions.map((a, idx) => (
+            <Grid key={idx} item xs={4} justifyContent="center">
+              <CardButton
+                onClick={() => {
+                  requestPepperText(a);
+                }}
+                background="#ccccff"
+              >
+                <Typography>{a}</Typography>
+              </CardButton>
+            </Grid>
+          ))}
+          <Grid item xs={12}>
+            <Divider />
+          </Grid>
+          <InsertTextField
+            value={notes}
+            hint="Send notes!"
+            background="#ccffcc"
+            onSubmit={() => {
               sendComment(notes);
               this.setState({ notes: "" });
-            },
-            (event) => this.setState({ notes: event.target.value })
-          )}
+            }}
+            onChange={(event) => this.setState({ notes: event.target.value })}
+          />
         </Grid>
         <Dialog
           open={this.state.dialogOpen}
